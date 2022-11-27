3d119035285089331e99258417617cfc2470cde6
Leticia <lettycuevas91@gmail.com>
2022-11-27 12:31:52 -0300

    Saludo

--- ejercicio6/index.js
+++ ejercicio6/index.js
@@ -7,5 +7,25 @@ const pedro = {
   
   console.log(pedro.edad);
   
+  pedro.estatura = 1.8;
+  
+  console.log(pedro);
+  
+  delete pedro.activo;
+  
+  console.log(pedro);
+  
+  
+for (let valor in pedro) {
+    if (pedro.hasOwnProperty(valor)) {
+      console.log(`{${valor}} : ${pedro[valor]}`);
+    }
+  }
+  
+  function saludo(nombre) {
+    return `Hola, me llamo ${nombre}`;
+  }
+  
+  console.log(saludo(pedro.nombre));
   
   
